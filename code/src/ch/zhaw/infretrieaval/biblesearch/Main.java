894dda567cde6dc96d8896e61d9e10052fb03526
Michael Hadorn <michael.hadorn@gmail.com>
2016-05-05 12:13:17 +0200

    initial code

---
+++ code/src/ch/zhaw/infretrieaval/biblesearch/Main.java
@@ -0,0 +1,12 @@
+package ch.zhaw.infretrieaval.biblesearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("huhu");
+
+        HelloLucene helloLucene = new HelloLucene();
+        // helloLucene.main();
+    }
+}
